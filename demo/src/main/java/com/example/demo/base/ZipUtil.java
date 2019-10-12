3c74d6e60f2ad71a20feeb4cb56c828025fc879d
dootto <642925243@qq.com>
2019-10-12 13:27:14 +0800

    添加zip包解压缩工具类

--- demo/src/main/java/com/example/demo/base/ZipUtil.java
+++ demo/src/main/java/com/example/demo/base/ZipUtil.java
@@ -1,7 +1,20 @@
 package com.example.demo.base;
 
+import org.apache.commons.compress.archivers.ArchiveEntry;
+import org.apache.commons.compress.archivers.ArchiveException;
+import org.apache.commons.compress.archivers.ArchiveInputStream;
+import org.apache.commons.compress.archivers.ArchiveStreamFactory;
+
+import java.io.*;
+import java.util.logging.FileHandler;
+
 public class ZipUtil {
 
+    /**
+     * 判断是否是zip文件
+     * @param fileNme
+     * @return
+     */
     private static boolean isZip(String fileNme) {
         boolean flag = false;
         if (fileNme != null) {
@@ -10,6 +23,29 @@ public class ZipUtil {
             }
         }
         return flag;
+    }
+
+    private static void decompressZip(String inputFile, String outputFile){
+        File file = new File(inputFile);
+        if (file.exists()){
+            InputStream in = null;
+            ArchiveInputStream s = null;
+            try {
+                in = new FileInputStream(file);
+                s =  new ArchiveStreamFactory().createArchiveInputStream("zip",in);
+                ArchiveEntry entry = null;
+                while ((entry = s.getNextEntry()) != null){
+                    String fileName = entry.getName();
+                }
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (ArchiveException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
